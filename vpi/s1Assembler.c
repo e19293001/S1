5195044f795b57d3d4427e22e5ff4f63afde0866
e19293001 <e19293001@gmail.com>
2016-02-08 12:49:21 +0800

    enabled access to argument from $s1Assembler function.
    No integration with the environment yet.
    the assembler will be tested first before integrating to the
    current environment.
    
    modified files:
    * vpi/makefile
      - icarus verilog vpi include path must now be declared in
        C_INCLUDE_PATH
      - added valgrind check. encoutered problems with icarus verilog:
        allocs != frees.
      - removed include makefrag and coptions
    
    * vpi/makefrag
      - removed all. this file should be deleted in the future
    
    * vpi/s1Assembler.c
      - added std libraries
      - argument check in s1AssemblerCompiletf
      - able to access the argument in s1AssemberCalltf
      - changed tab format.

--- vpi/s1Assembler.c
+++ vpi/s1Assembler.c
@@ -1,35 +1,86 @@
-# include  <vpi_user.h>
+#include <vpi_user.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void dumpVpiHandle(vpiHandle obj) {
+}
+
+static int s1AssemblerCompiletf(char*user_data)
+{
+  vpiHandle systf_handle;
+  vpiHandle arg_itr;
+  vpiHandle arg_handle;
+  PLI_INT32 tfarg_type;
+  int err_flag = 0;
+
+  systf_handle = vpi_handle(vpiSysTfCall, NULL);
+  arg_itr = vpi_iterate(vpiArgument, systf_handle);
+  if (arg_itr == NULL) {
+    vpi_printf("ERROR: $s1Assember requires 1 argument. has none.\n");
+    err_flag = 1;
+  }
+  arg_handle = vpi_scan(arg_itr);
+  tfarg_type = vpi_get(vpiType, arg_handle);
+  if (tfarg_type != vpiConstant &&
+      tfarg_type != vpiStringVal) {
+    vpi_printf("$s1Assember argument must be a string.\n");
+    err_flag = 1;
+  }
+  if (vpi_scan(arg_itr) != NULL) {
+    vpi_printf("ERROR: $s1Assember requires 1 argument. has too many.\n");
+  }
+
+  if (err_flag) {
+    vpi_control(vpiFinish, 1);
+  }
+
+  return 0;
+}
+
+static int s1AssemblerCalltf(char*user_data)
+{
+  s_vpi_value value_s;
+  vpiHandle systf_handle;
+  vpiHandle arg_itr;
+  vpiHandle arg_handle;
+  char *s;
+
+  systf_handle = vpi_handle(vpiSysTfCall, NULL);
+  if ((arg_itr = vpi_iterate(vpiArgument, systf_handle)) == NULL) {
+    vpi_printf("ERROR: $s1Assembler failed to obtain systf arg handle\n");
+    return(0);
+  }
+
+  arg_handle = vpi_scan(arg_itr);
+  value_s.format = vpiStringVal;
+  vpi_get_value(arg_handle, &value_s);
   
+  vpi_printf("hello string size: %0d\n", (int)strlen(value_s.value.str));
+  s = calloc(strlen(value_s.value.str), sizeof(char));
+  memset(s, '\0',strlen(value_s.value.str));
+  strncpy(s,value_s.value.str, strlen(value_s.value.str));
+  vpi_printf("argument: %s\n", s);
+
+  free(s);  
+  return 0;
 }
 
-static int vscaleAssembler_compiletf(char*user_data)
+void s1Assembler_register()
 {
-      return 0;
-}
+  s_vpi_systf_data tf_data;
 
-static int vscaleAssembler_calltf(char*user_data)
-{
-      vpi_printf("vscaleAssembler, World!\n");
-      vpi_printf("%s\n", user_data);
-      return 0;
-}
-
-void vscaleAssembler_register()
-{
-      s_vpi_systf_data tf_data;
-
-      tf_data.type      = vpiSysTask;
-      tf_data.tfname    = "$vscaleAssembler";
-      tf_data.calltf    = vscaleAssembler_calltf;
-      tf_data.compiletf = vscaleAssembler_compiletf;
-      tf_data.sizetf    = 0;
-      tf_data.user_data = 0;
-      vpi_register_systf(&tf_data);
+  tf_data.type      = vpiSysTask;
+  tf_data.sysfunctype = vpiSysFuncSized;
+  tf_data.tfname    = "$s1Assembler";
+  tf_data.calltf    = s1AssemblerCalltf;
+  tf_data.compiletf = s1AssemblerCompiletf;
+  tf_data.sizetf    = 0;
+  tf_data.user_data = 0;
+  vpi_register_systf(&tf_data);
 }
 
 void (*vlog_startup_routines[])() = {
-    vscaleAssembler_register,
-    0
+  s1Assembler_register,
+  0
 };
